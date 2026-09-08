c8d6746db38e65724df1318896df4c662c3b31fc
Yvon Meunier <meunieryvon6@gmail.com>
2026-09-08 11:54:53 -0400

    feat : shader loading

--- graphics/water-but-cooler/src/main.js
+++ graphics/water-but-cooler/src/main.js
@@ -3,16 +3,32 @@ import * as THREE from './vendor/three/build/three.module.js';
 const width = window.innerWidth, height = window.innerHeight;
 
 // init
-
+const loader = new THREE.FileLoader();
 const camera = new THREE.PerspectiveCamera( 70, width / height, 0.01, 10 );
 camera.position.z = 1;
 
 const scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+const geometry = new THREE.PlaneGeometry(1,1);
+
+const [vertexShader, fragmentShader] = await Promise.all([
+    loader.loadAsync('./src/shaders/water.vert'),
+    loader.loadAsync('./src/shaders/water.frag'),
+]);
+
+
+//const material = new THREE.MeshBasicMaterial( { color: 0xffff00, side: THREE.FrontSide } );
+const material = new THREE.ShaderMaterial({
+	uniforms: {
+      uTime: { value: 0 },
+      uColor: { value: new THREE.Color(0xffff00) },
+    },
+	fragmentShader : fragmentShader,
+	vertexShader : vertexShader
+});
 
 const mesh = new THREE.Mesh( geometry, material );
+
 scene.add( mesh );
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -22,11 +38,10 @@ document.body.appendChild( renderer.domElement );
 
 // animation
 
+mesh.rotateX(-Math.PI/4);
+
 function animate( time ) {
-
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
-
+	material.uniforms.uTime.value = time / 1000;
 	renderer.render( scene, camera );
 
 }
---
+++ graphics/water-but-cooler/src/shaders/water.frag
@@ -0,0 +1,6 @@
+uniform vec3 uColor;
+uniform float uTime;
+varying vec2 vUv;
+void main() {
+  gl_FragColor = vec4(uColor * (0.5 + 0.5 * sin(uTime + vUv.x * 6.28)), 1.0);
+}
